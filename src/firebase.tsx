a38b53c1e47059c1e772f8876917241f98307f25
Ritesh Sharma <45563022+ritesh-sharma33@users.noreply.github.com>
2020-09-16 21:02:22 +0530

    Updated firebase.ts

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -5,14 +5,14 @@ import config from "./config";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: config.apiKey,
-    authDomain: config.authDomain,
-    databaseURL: config.dbUrl,
-    projectId: config.projectId,
-    storageBucket: config.storageBucket,
-    messagingSenderId: config.msgSenderId,
-    appId: config.appId,
-    measurementId: config.measurementId
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DB_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 try {
